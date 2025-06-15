6a256a5d9a841e228b81dda63e0d57064e2fec26
vanya-avdoshko <ivanavdoshko2@gmail.com>
2025-06-15 21:48:14 +0300

    fix: changed header and logo

--- src/components/layout/HeaderComponent/Header.jsx
+++ src/components/layout/HeaderComponent/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import logo from './logo192.png';
+import styles from './header.module.css';
 
 const Header = () => {
-    return (
-        <header> 
-            <div className='company_logo'>
-                <img src={logo} title='иконка предприятия'/> 
-            </div>
-
-            <div className='company_title'>
-                <h1> СООО БИК СЗМА </h1>
-                <h2>Белорусская инжиниринговая компания</h2> 
-                <hr/>
-                <h1>СЕВЗАПМОНТАЖАВТОМАТИКА</h1>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <img src="/images/biklogo3.gif" alt="Логотип предприятия" className={styles.logo} />
+      </div>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.companyName}>СООО БИК СЗМА</h1>
+        <h2 className={styles.companyDescription}>Белорусская инжиниринговая компания</h2>
+        <hr className={styles.divider} />
+        <h1 className={styles.companyName}>СЕВЗАПМОНТАЖАВТОМАТИКА</h1>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
